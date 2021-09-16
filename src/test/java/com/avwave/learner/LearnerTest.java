477e9e0ada388106160a9d44f5d4b0e0fd8a938d
Ray Arvin Rimorin <rayarvin@gmail.com>
2021-09-16 03:13:01 +0000

    Initial commit

---
+++ src/test/java/com/avwave/learner/LearnerTest.java
@@ -0,0 +1,21 @@
+package com.avwave.learner;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class LearnerTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
